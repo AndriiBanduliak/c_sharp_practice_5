1e1786c9000ba5e0fae52a36f52804f4e8cbc79b
Andrii Banduliak <banduliak1303@gmail.com>
2022-11-25 20:27:18 +0300

    task #32 is done

--- Program.cs
+++ Program.cs
@@ -1,1 +1,26 @@
 ﻿
+using static System.Console;
+Clear();
+
+int[] array = GetArray(8);
+WriteLine($"{String.Join(" ", array)}");
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] *= (-1);
+}
+WriteLine($"{String.Join(" ", array)}");
+
+
+
+
+int[] GetArray(int L)
+{
+    int[] result = new int[L];
+    for (int i = 0; i < result.Length; i++)
+    {
+        result[i] = new Random().Next(-9, 10);
+    }
+
+    return result;
+}
